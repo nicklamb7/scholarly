6780a9e65c7552ae35264cb81e982701564da253
Nicholas Lamb <nlamb@hotmail.co.uk>
2021-08-24 00:40:03 +0100

    add controller and view for schools

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # Display all
+  get "schools", to: "schools#index"
+  # Display one
+  get "schools/:id", to: "schools#show", as: :school
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'Creating 100 fake schools...'
+100.times do
+  school = School.new(
+    name:    Faker::Educator.secondary_school,
+    school_type: "Academy",
+    address: Faker::Address.full_address,
+    summary: Faker::Lorem.paragraph(sentence_count: 3),
+    founded_at:  rand(1440..2000),
+    price: rand(0..15000),
+    number_of_pupils: rand(100..1800),
+    grade_5_or_above: rand(40..100),
+    aab_or_above: rand(40..100),
+    latitude: Faker::Address.latitude,
+    longitude: Faker::Address.longitude,
+  )
+  school.save!
+end
+puts 'Finished!'
