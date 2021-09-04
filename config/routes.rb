9a0e212ee7776b4a253d7ec99d19b30a0b057234
Nicholas Lamb <nlamb@hotmail.co.uk>
2021-09-04 15:47:07 +0100

    add create, update, destroy actions to schools controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: "pages#home"
   resources :schools do
     resources :reviews, only: [ :new, :create ]
+    resources :destinations, only: [ :index ]
   end
   resources :reviews, only: [ :destroy ]
 end
