5f439f42c7d2cd549c0526ef30da635d5a3148b4
Nick Lamb <nlamb@hotmail.co.uk>
2021-09-13 18:17:46 +0100

    Add rating to Reviews

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_13_160916) do
+ActiveRecord::Schema.define(version: 2021_09_13_171527) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 2021_09_13_160916) do
     t.bigint "school_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "rating"
     t.index ["school_id"], name: "index_reviews_on_school_id"
   end
 
