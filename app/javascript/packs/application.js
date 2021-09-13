7fb991db7ee6615f30404f2ce52d9697c3202162
Nick Lamb <nlamb@hotmail.co.uk>
2021-09-13 18:52:23 +0100

    add ratings to school show page.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,7 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-
+import "controllers"
 import "chartkick/chart.js"
 import "chartkick"
 import "chartkick/highcharts"
@@ -10,11 +10,10 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-
+import { initStarRating } from '../plugins/init_star_rating';
 import { initMapbox } from '../plugins/init_mapbox';
 
 document.addEventListener('turbolinks:load', () => {
   initMapbox();
+  initStarRating();
 })
-
-import "controllers"
